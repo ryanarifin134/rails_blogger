1322851a02b497bdbd5d3a0b45d9ca793746fd1f
Ryan Arifin <ryanarifin134@gmail.com>
2019-05-31 02:43:59 -0500

    Messed around with Paperclip (image library) to add images to blog posts

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,9 @@ class Article < ApplicationRecord
   has_many :taggings
   has_many :tags, through: :taggings
   has_many :comments
+  has_attached_file :image
+  validates_attachment_content_type :image,
+                                    :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
   # Returns a comma separated list of the article's tags
   def tag_list
