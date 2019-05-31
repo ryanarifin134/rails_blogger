d584b67f2e711f2c5ac7ed17f950a990cfaa431e
Ryan Arifin <ryanarifin134@gmail.com>
2019-05-31 00:58:35 -0500

    Partly implemented tagging system

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,21 @@
 class Article < ApplicationRecord
+  has_many :taggings
+  has_many :tags, through: :taggings
   has_many :comments
+
+  # Returns a comma separated list of the article's tags
+  def tag_list
+    tags.join(", ")
+  end
+
+  def tag_list=(tags_string)
+    tags = tags_string.split(',')
+            .collect { |tag| tag.strip.downcase }.uniq
+
+    new_or_found_tags = tags.collect do |tag_name|
+      Tag.find_or_create_by(name: tag_name)
+    end
+
+    self.tags = new_or_found_tags
+  end
 end
